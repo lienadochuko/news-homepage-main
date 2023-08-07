0d98513eeab5700295d1cf0f2e63da3029607157
lienadochuko <erhisowomedanielking@gmail.com>
2023-08-07 20:23:37 +0100

    grid columns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import logo from '../src/assets/logo.svg';
 import './App.css';
 import retro from '../src/assets/image-retro-pcs.jpg';
 import laptop from '../src/assets/image-top-laptops.jpg';
 import game from '../src/assets/image-gaming-growth.jpg';
+import menuopen from '../src/assets/icon-menu.svg'
+import close from '../src/assets/icon-menu-close.svg'
 
 function App() {
+  const [menu, setMenu] = useState(false);
+  
+  const open = () => {
+    setMenu(!menu);
+    console.log(menu);
+}
   return (
     <div className="App">
       <div className='navbar'>
@@ -14,6 +23,18 @@ function App() {
         <div className='item'>Popular</div>
         <div className='item'>Trending</div>
         <div className='item'>Categories</div>
+
+        <img src={menuopen} alt="menuIcon" className="menuIcon" onClick={open}/>              
+        <div className="menu" style={{display: menu ? 'flex' : 'none'}}> 
+            <img src={close} alt="closeIcon" className="close" onClick={open}/>
+            <div className="boxhold">
+            <div className="box">Home</div>
+            <div className="box">News</div>
+            <div className="box">Popular</div>
+            <div className="box">Trending</div>
+            <div className="box">Categories</div>
+            </div>
+        </div>
         </div>
       <div className='wrapper'>
         <div className='left'>
@@ -41,19 +62,36 @@ function App() {
           the possible adverse effects of on-demand AI image generation?</span></div>
           <div className='newsWrapper'> <span className='title'>Is VC Funding Drying Up?</span>
           <span className='body'>Private funding by VC firms is down 50%
-           YOY. We take a look at what that means</span></div>          
+           YOY. We take a look at what that means.</span></div>          
           </div>     
         </div>
       </div>
       <div className='bottomWrapper'>
         <div className='bottomItem'>
           <img src={retro} alt='retro' className='imagine'/>
+          <div className='bottomContainer'>
+            <span className='bottomNumber'>01</span>
+            <span className='bottomTitle'>Reviving Retro PCs</span>
+            <span className='bottomBody'>What happens when old PCs are 
+              given modern upgrades?</span>
+          </div>
         </div>
         <div className='bottomItem'>
           <img src={laptop} alt='laptop'  className='imagine'/>
+          <div className='bottomContainer'>
+            <span className='bottomNumber'>02</span>
+            <span className='bottomTitle'>Top 10 Laptops of 2022</span>
+            <span className='bottomBody'>Our best picks
+             for various needs and budgets.</span>
+          </div>
         </div>
         <div className='bottomItem'>
           <img src={game} alt='game'  className='imagine'/>
+          <div className='bottomContainer'>
+            <span className='bottomNumber'>03</span>
+            <span className='bottomTitle'>The Growth of Gaming</span>
+            <span className='bottomBody'>How the pandemic has sparked fresh opportunities.</span>
+          </div>
           </div>
       </div>
     </div>
